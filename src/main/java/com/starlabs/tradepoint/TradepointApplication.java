bc8085a3b654a6966d747b2433a7238dc03596f5
= <=>
2020-06-07 00:01:22 +0530

    Initial project start commit

---
+++ src/main/java/com/starlabs/tradepoint/TradepointApplication.java
@@ -0,0 +1,13 @@
+package com.starlabs.tradepoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradepointApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradepointApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/starlabs/tradepoint/TradepointApplicationTests.java
@@ -0,0 +1,13 @@
+package com.starlabs.tradepoint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradepointApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
